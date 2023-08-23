2af23de829698ffebd64cc3cef951676258b24eb
Rafael Melo <124813972+rafaelnamel0@users.noreply.github.com>
2023-08-23 12:54:10 -0300

    Add files via upload

---
+++ d1di2lzuh97fh2.cloudfront.net/client.fe/js.compiled/lang.pt-br.1223.js
@@ -0,0 +1,14 @@
+// Copyright 2006 The Closure Library Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+window.translations = ["Grátis", "Mini", "Standard", "Profissional", "Limited", "Business", "Blog", "Produtos", "Por enquanto não há nenhum post. Crie um clicando em \"Novo post\".", "Não há produtos na loja até ao momento. Comece por clicar em \"Adicionar produto\".", "Por enquanto não há nenhuns posts. Comece por adicionar página \"Blog\".", "Esta categoria está vazia. Adicione produtos nela ou selecione outra categoria.", "Galeria de fotos", "Não foram encontradas imagens nesta galeria", "In stock", "Available in 3 days", "Available in 7 days", "Available in 14 days", "Available in 1 month", "Available in more than 1 month"];
